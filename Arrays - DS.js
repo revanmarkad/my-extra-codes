17a41753368fea9bd9c32c1f3a93d46d51c1aca9
Revan Markad <125389181+revanmarkad@users.noreply.github.com>
2023-04-17 08:56:12 +0530

    Add files via upload

---
+++ Arrays - DS.js
@@ -0,0 +1,65 @@
+// HackerRank
+
+// input 
+// A=[1,2,3];
+
+// Output 
+// A=[3,2,1];
+
+
+
+
+
+
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'reverseArray' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function reverseArray(a) {
+    a.reverse(a)
+    
+   return a;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const arrCount = parseInt(readLine().trim(), 10);
+
+    const arr = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    const res = reverseArray(arr);
+
+    ws.write(res.join(' ') + '\n');
+
+    ws.end();
+}
